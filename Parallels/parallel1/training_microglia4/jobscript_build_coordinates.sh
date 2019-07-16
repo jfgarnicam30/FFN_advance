cadb2786bf1db549699226ba0eaebab8db0f668a
jfgarnicam30 <46555355+jfgarnicam30@users.noreply.github.com>
2019-07-16 10:37:32 +0300

    Add files via upload

---
+++ Parallels/parallel1/training_microglia4/jobscript_build_coordinates.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH -N 1
+##SBATCH --partition=debug
+#SBATCH --partition=batch
+#SBATCH -J build_all_l0_1
+#SBATCH -o build_all_l0_1.%J.out
+#SBATCH -e build_all_l0_1.%J.err
+#SBATCH --mail-user=javierfernando.garnicamolina@kaust.edu.sa
+#SBATCH --mail-type=ALL
+#SBATCH --time=5-00:00:00
+#SBATCH --mem=116G
+#SBATCH --gres=gpu:1
+#SBATCH --constraint=[v100]
+
+
+module load anaconda3
+source activate /home/garnicj/ksl-fernando/fernando_env/
+
+
+#run the application
+/home/garnicj/ksl-fernando/fernando_env/bin/python -u /ibex/scratch/garnicj/parallel1/training_microglia4/script_coordinates.py
---
+++ Parallels/parallel6/training_microglia4/script_coordinates.py
@@ -0,0 +1,20 @@
+import os
+
+print(' \n' + ' \n' + "Build coordinates starting" + ' \n' + ' \n')
+
+layer = 1
+raw = 7
+column = 4
+
+path = "/ibex/scratch/garnicj/parallel6"
+constant_path = "/ibex/scratch/garnicj/parallel2"    
+
+for i in range(1):
+        for j in range(4):
+                line = str(layer) + '_' + str(raw+i) + '_' + str(column+j) + '_' 
+                print("working on: " + line + ' \n' + ' \n')
+                command = ("/home/garnicj/ksl-fernando/fernando_env/bin/python -u " + path + "/ffn-master/build_coordinates.py \
+--partition_volumes validation1:" + constant_path + "/training_microglia4/af_all_labels/af_all_labels_" + line +".h5:af \
+--coordinate_output " + constant_path + "/training_microglia4/tf_record_all_labels/tf_record_all_labels_" + line +" \
+--margin 24,24,24")
+                os.system(command)
